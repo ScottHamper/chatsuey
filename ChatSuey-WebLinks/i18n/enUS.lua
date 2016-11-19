f0d778bc44e0013c837e0b85f48723b1615d06cb
Scott Hamper <Scott.Hamper@gmail.com>
2016-11-18 23:29:59 -0500

    Fixed bad reference to _G

--- ChatSuey-WebLinks/i18n/enUS.lua
+++ ChatSuey-WebLinks/i18n/enUS.lua
@@ -2,6 +2,6 @@
 -- how to implement a locale, as well as being a list of all the
 -- localized strings used.
 
-local LS = _G.ChatSuey.Locales["enUS"].Strings;
+local LS = ChatSuey.Locales["enUS"].Strings;
 
 LS["Copy the URL into your clipboard (Ctrl-C):"] = "Copy the URL into your clipboard (Ctrl-C):";
